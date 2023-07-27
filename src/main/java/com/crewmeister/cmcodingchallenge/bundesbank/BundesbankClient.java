744c2f3c35a9900ba7ea4caf282f086e5311df91
Casper Lans <casper.lans@zuehlke.com>
2023-07-27 22:38:20 +0200

    Increase WebClient codec max size

--- src/main/java/com/crewmeister/cmcodingchallenge/bundesbank/BundesbankClient.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/bundesbank/BundesbankClient.java
@@ -3,15 +3,23 @@ package com.crewmeister.cmcodingchallenge.bundesbank;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class BundesbankClient {
 
+    private static final int MAX_CODEC_BYTES = 100 * 1024 * 1024;
+
     private final WebClient client;
 
     BundesbankClient() {
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(codecs -> codecs.defaultCodecs().maxInMemorySize(MAX_CODEC_BYTES))
+                .build();
+
         client = WebClient.builder()
+                .exchangeStrategies(strategies)
                 .baseUrl("https://api.statistiken.bundesbank.de/rest/data")
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .build();
@@ -19,8 +27,7 @@ public class BundesbankClient {
 
     String getCurrenciesJson() {
         return client.get()
-                // TODO: remove `?lastNObservations=10`
-                .uri("/BBEX3/D..EUR.BB.AC.000?lastNObservations=10")
+                .uri("/BBEX3/D..EUR.BB.AC.000")
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
