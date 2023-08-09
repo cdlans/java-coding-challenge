337260988c731cafec1ed4fe63d881627c67a543
Casper Lans <casper.lans@zuehlke.com>
2023-08-09 20:14:10 +0200

    Remove unused methods from RateRepository

--- src/main/java/com/crewmeister/cmcodingchallenge/domain/RateRepository.java
+++ src/main/java/com/crewmeister/cmcodingchallenge/domain/RateRepository.java
@@ -6,23 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface RateRepository extends Repository<Rate, Long> {
-
-    Page<Rate> findAll(Pageable page);
 
     Page<Rate> findAll(Example<Rate> rateExample, Pageable pageable);
 
     Optional<Rate> findById(Long id);
 
-    Optional<Rate> findByCurrencyAndDate(String currency, LocalDate date);
-
     Rate save(Rate rate);
-
-    List<Rate> saveAll(Iterable<Rate> rates);
 
     @Query("SELECT DISTINCT r.currency FROM Rate r")
     Page<String> findAllCurrencies(Pageable page);
